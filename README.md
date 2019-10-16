566537dea054aeb4b023a69a2f0f432b8ff6879a
Mark Chadwick <m.chadwick@gns.cri.nz>
2019-10-16 17:38:56 +1300

    add keys

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # test
+
+$ gem install travis
+$ ssh-keygen -t rsa -b 4096 -C "ah@be.com"
+
+Add to public key to Deploy Keys on the repo
+
+password=$(openssl rand -hex 32)
+cat travis_key | openssl aes-256-cbc -k "$password" -md sha256 -a  > travis_key.enc
+
+travis encrypt travis_key_password=$password --add env.matrix
+
+git add travis_key.enc
+git add .travis.yml
+git commit -m ...
+
+then in build script
+
+openssl aes-256-cbc -k "$travis_key_password" -d -md sha256 -a -in travis_key.enc -out travis_key
+echo "Host github.com" > ~/.ssh/config
+echo "  IdentityFile  $(pwd)/travis_key" >> ~/.ssh/config
+chmod 400 travis_key
--- build/build.sh
+++ build/build.sh
@@ -9,11 +9,16 @@ error_handler () {
 
 trap error_handler ERR
 
+git checkout -b build
+git remote set-url self git@github.com:ozym/test.git
+openssl aes-256-cbc -k "$travis_key_password" -d -md sha256 -a -in travis_key.enc -out travis_key
+echo "Host github.com" > ~/.ssh/config
+echo "  IdentityFile  $(pwd)/travis_key" >> ~/.ssh/config
+chmod 400 travis_key
+echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" > ~/.ssh/known_hosts
+
 go get github.com/GeoNet/delta/meta
 go get gopkg.in/yaml.v2
-
-git checkout -b build
-git remote add upstream git@github.com:ozym/test.git
 
 mkdir -p files/ntripcaster
 go build ./code/cmd/ntripcaster-config || exit 255
@@ -23,7 +28,7 @@ go build ./code/cmd/ntripcaster-config || exit 255
 git add files/ntripcaster/test.yaml
 git commit -m 'auto update [skip travis]'
 git remote -v 
-git push upstream build:master
+git push self build:master
 
 exit $errcount
 
